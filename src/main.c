faedce221b3ec79f1d03db7c50320cb3a141cb3d
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-06-11 22:34:18 -0600

    Fixed header file importation
    So, if you write it "like this" it means you're trying to include a file that's somewhere in the repository evoked using the filepath, whereas when it's <like this> you're calling either the standard library or some header that's installed globally somewhere on your system

--- src/main.c
+++ src/main.c
@@ -1,11 +1,7 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "../include/glad/glad.h"
+#include <stdio.h>
 
-
-int main() {
-
-    
-
-
-    return 0;
+int main(void) {
+  printf("Hi!\n");
+  return 0;
 }
